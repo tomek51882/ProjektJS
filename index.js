541a6ed343dc7bdc35a1e5872d23d1942aeb461f
tomek51882 <tomek51882@gmail.com>
2020-01-30 00:15:32 +0100

    ChannelChat
    Changing the project: Battleships was too complicated for given time.
    
    Added chatting with someone. After connection user need to select channel.
    
    Channel One is working OK.
    Channel Two and Three is bugged.

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
-    });
+  socket.on('chat message', function(msg){
+    if(io.sockets.adapter.sids[socket.id]['room1'])
+    {
+      socket.to('room1').emit('chat message', "Guess: "+msg);
+    }
+    if(io.sockets.adapter.sids[socket.id]['room2'])
+    {
+      socket.to('room2').emit('chat message', "Guess: "+msg);
+    }
+    if(io.sockets.adapter.sids[socket.id]['room2'])
+    {
+      socket.to('room3').emit('chat message', "Guess: "+msg);
+    }
+    socket.emit('chat message', "Me: " + msg);
   });
+  socket.on('ch1', function(msg){
+    if(io.sockets.adapter.sids[socket.id]['room2'])
+    {
+      socket.leave('room2');
+    }
+    if(io.sockets.adapter.sids[socket.id]['room3'])
+    {
+      socket.leave('room3');
+    }
+    socket.join('room1');
+  });
+  socket.on('ch2', function(msg){
+    if(io.sockets.adapter.sids[socket.id]['room1'])
+    {
+      socket.leave('room1');
+    }
+    if(io.sockets.adapter.sids[socket.id]['room3'])
+    {
+      socket.leave('room3');
+    }
+    socket.join('room2');
+  });
+  socket.on('ch3', function(msg){
+
+  });
+});
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
